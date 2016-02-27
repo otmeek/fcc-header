9f998ad0db69edbb557090f59bd1a4cf5181e61b
Mafalda Pereira <otmeek@gmail.com>
2016-02-27 22:29:24 +0000

    finish project

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 'use strict';
 
 var express = require('express');
-var routes = require('./app/routes/index.js');
 var mongoose = require('mongoose');
 var passport = require('passport');
-var session = require('express-session');
 
 var app = express();
 require('dotenv').load();
@@ -12,20 +10,37 @@ require('./app/config/passport')(passport);
 
 mongoose.connect(process.env.MONGO_URI);
 
-app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/common', express.static(process.cwd() + '/app/common'));
+app.get('/', function(req, res) {
+	var ua   = req.headers['user-agent'];
+	var lang = req.headers["accept-language"];
+	
+	var obj = {
+		"ipaddress": undefined,
+		"language" : undefined,
+		"software" : undefined
+	};
+	
+	// software
+	var regex  = new RegExp(/\(/);
+	var regex2 = new RegExp(/\)/);
+	var startIndex = ua.search(regex);
+	var endIndex   = ua.search(regex2);
+	var software = ua.substring(startIndex+1, endIndex);
+	obj["software"] = software;
+	
+	// language
+	var regex3 = new RegExp(/,/);
+	var langEndIndex = lang.search(regex3);
+	var language = lang.substring(0, langEndIndex);
+	obj["language"] = language;
+	
+	// ip
+	var ip = req.headers['x-forwarded-for'];
+	obj["ipaddress"] = ip;
+	
+	res.send(JSON.stringify(obj));
+})
 
-app.use(session({
-	secret: 'secretClementine',
-	resave: false,
-	saveUninitialized: true
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-routes(app, passport);
 
 var port = process.env.PORT || 8080;
 app.listen(port,  function () {
